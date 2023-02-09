cb2df7abe5b40563b713b0662bb7d73c1ba29610
Milan Zekić <milanzekic@hotmail.rs>
2023-02-09 17:57:56 +0100

    first test

--- my-project/src/test/java/Tests.java
+++ my-project/src/test/java/Tests.java
@@ -1,40 +1,39 @@
-
-import org.junit.Assert;
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class Tests {
     WebDriver driver;
 
     @BeforeTest
     public void setup() {
-      
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\milan\\OneDrive\\Radna površina\\AutomatedTests\\chromedriver");
+        
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\milan\\OneDrive\\Radna površina\\AutomatedTests\\chromedriver.exe");
+        
+        
+        driver = new ChromeDriver();
+        
+        
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
         
        
-        driver = new ChromeDriver();
-       driver.get("https://www.example.com");
-        
-        
         driver.manage().window().maximize();
-        
-  
     }
 
     @Test
     public void verifyHomePage() {
-    	
+        
         String actualTitle = driver.getTitle();
-        String expectedTitle = "Example - Home Page";
+        String expectedTitle = "GreenKart - veg and fruits kart";
         Assert.assertEquals(actualTitle, expectedTitle, "User has not landed on the home page");
     }
 
     @AfterTest
     public void teardown() {
-       
+        
         driver.quit();
     }
 }
